6e6037181d0e8e42e3bb04173dfd5ebf02316cb7
bilal <bilal.hkmsh@gmail.com>
2022-12-01 17:21:19 +0300

    first commit

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
-/* //How to create a simple server :) 
+ //How to create a simple server :) 
 const http = require('http');
+const port='3000';
+const server= http.createServer((req,res)=>{
+    if (req.url == '/'){
+        res.statusCode=200;
+        return res.end('<h1>Hello NodeJS</h1>');
+    }else if (req.url=='/about') {
+        res.statusCode=200;
+        return res.end('<h1>Hello from about</h1>');
+    }else{
+        res.statusCode=404
+        return res.end('<h1>Error 404</h1>')
+    }
 
-const server = http.createServer((req,res)=>{
-    res.write('hello node');
-    res.end();
 })
-server.listen('3000',()=>{console.log(`server is running.. http://localhost:3000`);}); */
 
-
-
+server.listen(port,()=>{
+    console.log(`server is running... http://localhost:${port}`);
+})
