050b2004d119596ede085aad3f90776623d7f89a
bilal <bilal.hkmsh@gmail.com>
2022-12-01 18:05:42 +0300

    first commit

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
- //How to create a simple server :) 
+ // create a simple server with if and html :) 
 const http = require('http');
 const port='3000';
+const fs = require('fs');
+
+homePage=fs.readFileSync('./pages/home.html')
+aboutPage=fs.readFileSync('./pages/about.html')
+errorPage=fs.readFileSync('./pages/error.html')
+
 const server= http.createServer((req,res)=>{
     if (req.url == '/'){
         res.statusCode=200;
-        return res.end('<h1>Hello NodeJS</h1>');
+        //return res.end('<h1>Hello NodeJS</h1>');
+        return res.end(homePage);
     }else if (req.url=='/about') {
         res.statusCode=200;
-        return res.end('<h1>Hello from about</h1>');
+        //return res.end('<h1>Hello from about</h1>');
+        return res.end(aboutPage)
     }else{
         res.statusCode=404
-        return res.end('<h1>Error 404</h1>')
+        //return res.end('<h1>Error 404</h1>')
+        return res.end(errorPage)
     }
 
 })
