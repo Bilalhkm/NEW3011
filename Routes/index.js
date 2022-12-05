3d4788f73e5a8a44c4634da42ed3338695e69f47
Bilal HEKİMOĞLU <bilal.hkmsh@gmail.com>
2022-12-05 14:04:18 +0300

    https://github.com/Bilalhkm/NEW3011

--- Routes/index.js
+++ Routes/index.js
@@ -2,11 +2,12 @@ const express = require('express');
 router= express.Router();
 
 router.get('/',(req,res)=>{
-    res.sendfile('pages/home.html');
+    res.sendfile('./pages/home.html');
     
 });
+
 router.all('/*',(req,res)=>{
-    res.status(404).send("Oh uh, something went wrong");
+    res.status(404).sendfile("./pages/error.html");
 })
 
 module.exports=router;
